c7baad8cdf22a58cbf90dd47d0f1808ac5ee9368
Alejandro Montero <alejandro.montero@correounivalle.edu.co>
2024-05-15 23:25:15 -0500

    creando los roles con la libreria laravel-permission

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+// use Spatie\Permission\Models\role;
 use Illuminate\Support\Facades\Route;
 
+// $role = Role::create(['name' => 'admin']);
+// $role = Role::create(['name' => 'client']);
 Route::get('/', function () {
     return view('welcome');
 });
